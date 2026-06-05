8ed10c485d6f40ef67a830d42beb1ad439a76538
Igor <igor.triva@email.com>
2026-06-05 21:25:04 +0200

    Add PostHog analytics (web)

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import posthog from "posthog-js";
 
 Sentry.init({
   dsn: "https://7d96f7464b04a409c8e48593145bbbe3@o4511503641739264.ingest.de.sentry.io/4511503655239760",
@@ -15,6 +16,11 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+});
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: '2026-01-30',
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
